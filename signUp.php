517fe9306723af22b64a4c577cd155309d3bf2bb
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-04-27 15:27:58 -0700

    Added the form for Sign Up

--- signUp.php
+++ signUp.php
@@ -4,7 +4,7 @@
     require_once('connect.php');
 
     if (@$_POST['signUp']) {
-        
+
     }
 ?>
 
@@ -16,6 +16,25 @@
     </head>
 
     <body>
+        <h3>Sign Up</h3>
 
+        <form method="post" name="signUp">
+            <input type="text" name="username">
+            <input type="email" name="email">
+            <input type="text" name="address">
+            <input type="number" name="phoneNumber">
+            <input type="number" name="cardNumber">
+            <select name="catagory">
+                <option value="Auto">Auto</option>
+                <option value="Clothing">Clothing</option>
+                <option value="Technology">Technology</option>
+                <option value="Household">Household</option>
+                <option value="Games">Games</option>
+                <option value="Tools">Tools</option>
+                <option value="Sport">Sport</option>
+            </select>
+            <input type="password" name="password">
+            <input type="password" name="confirmPassword">
+        </form>
     </body>
 </html>
