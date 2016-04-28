3e19105103f8e64512fb8ef9ba1f9623d9c81499
Eric <eric-pierce@west-mec.org>
2016-04-28 15:24:22 -0700

    End of 4-28-2016 push

--- index.php
+++ index.php
@@ -26,19 +26,30 @@ foreach ($id as $row) {
     echo '<tr>';
     echo '<strong>Name:</strong> ' . $row['productName'] . '<br />';
     echo '<strong>Cost:</strong> ' . $row['productPrice'] . '<br />';
-    echo '<strong>Description:</strong></tr>' .$row['productLikes'] .'<br />';
-    echo '<strong>Likes:</strong></tr>' .$row['productDescription'] .'<br />';
-    echo '<strong>Seller:</strong></tr>' .$row['users_username'] .'<br />';
+    echo '<strong>Description:</strong>' .$row['productLikes'] .'<br />';
+    echo '<strong>Likes:</strong>' .$row['productDescription'] .'<br />';
+    echo '<strong>Seller:</strong>' .$row['users_username'] .'<br />';
+    echo '<strong>Catagorie:</strong>'.$row['productCategorie']. '<br />'
     if (is_file($filepath) && filesize($filepath) > 0) {
-        echo '<td><img src="' . $filepath . '"alt="Find Image"  class="image"/></td></tr>';
+        echo '<td><img src="' . $filepath . '"alt="Find Image"  class="image"/></td>';
         echo '</table>';
     }
 }
 // If the user is signed in then the index will display 10 products to only
-// show items from the categories that the user who is signed in/ logged in likes
+// Show items from the categories that the user who is signed in/ logged in likes
 if (isset($_SESSION['username'])) {
     echo ' <a href="profile.php">View Profile</a><br />';
     echo ' <a href="logout.php">Log Out (' . $_SESSION['username'] . ')</a>';
+    //Show 10 products tied to the categories that the user picked
+    $query = "SELECT * FROM products ORDER BY id DESC  LIMIT 10 ";
+    $stmt =  $dbh->prepare($query);
+    $stmt->execute();
+    $data = $stmt->fetchAll();
+    // if you picked something you see that something
+
+    // if (???) {
+    // php stuff echo
+    // }
 }
 
 ?>
