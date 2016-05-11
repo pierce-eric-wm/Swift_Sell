350f0311d36de54024588d82e64caedb68b39e94
Jose Gonzalez <session1@START-Coding-MBP-01.local>
2016-05-11 14:35:39 -0700

    index styling

--- index.php
+++ index.php
@@ -103,20 +103,37 @@
                     foreach ($products as $row) {
                         $imagePath = "images/" . $row['productImage'];
 
-                        echo '<div class="product">';
-                        echo '<img' . " " . 'width="' . '100%"' . " " . 'src="'. $imagePath .'"/>';
-                        echo "<br>";
-                        echo '<p>' . $row['productName'] . '</p>';
-                        echo "<br>";
-                        echo '<p>' . $row['productPrice'] . '</p>';
-                        echo "<br>";
-                        echo '<p>' . $row['productDescription'] . '</p>';
-                        echo "<br>";
-                        echo '<p>' . $row['productCatagory'] . '</p>';
-                        echo "<br>";
-                        echo '<p>' . $row['productLikes'] . '</p>';
-                        echo "<br>";
-                        echo '<p>' . $row['users_username'] . '</p>';
+                        echo '<div class="productholder2">';
+
+                            echo '<p>' . $row['users_username'] . '</p>';
+
+                            echo '<div class="imgholder">';
+
+                                echo '<img src="'. $imagePath .'" style="height: 160px; width: 200px; />';
+
+                            echo '</div>';
+
+
+                            echo '<div class="nameholder">';
+                                echo '<p>' . '<b>' . $row['productName'] . '</b>' . '</p>';
+                            echo "</div>";
+
+                            echo '<div class="priceholder">';
+                                echo '<p>' . $row['productPrice'] . '</p>';
+                            echo "</div>";
+
+                            echo '<div class="likeholder">';
+                                echo '<p>' . $row['productLikes'] . '</p>';
+                            echo "</div>";
+
+                            echo '<div class="categoryholder">';
+                                echo '<p>' . $row['productCatagory'] . '</p>';
+                            echo "</div>";
+
+                            echo '<div class="descriptionholder">';
+                                echo '<p>' . $row['productDescription'] . '</p>';
+                            echo "</div>";
+
                         echo '</div>';
                     }
                     echo '<div style="clear: both;"</div>';
