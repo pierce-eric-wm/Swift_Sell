6bc0c7a5944afa68f308f002a0c9bacb99b0a33d
Ariel <ariel.steele@west-mec.org>
2016-05-18 14:22:44 -0700

    Profile Page.

--- profile.php
+++ profile.php
@@ -6,20 +6,93 @@
     </head>
 
     <body>
-    <a href="index.php">Swift Sell</a>
-    <br>
-    <a href="profile.php">Profile</a>
-    <br>
-    <a href="editProfile.php">Edit Profile</a>
-    <br>
-    <a href="signIn.php">Sign In</a>
-    <br>
-    <a href="signOut.php">Sign Out</a>
-    <br>
-    <a href="signUp.php">Sign Up</a>
-    <br>
-    <a href="upload.php">Upload</a>
-    <br>
+    <div class="casingnav">
+        <div class="topbar">
+            <div class="casingnav">
+                <ul>
+                    <a href="index.php"><img src="images/SwiftSell.png" style="height:58px; float: left"> </a>
+                    <p class="locationlink" style="margin-left: 10px;"><a href="Home.php">Phoenix, Az <span class="glyphicon glyphicon-map-marker"> </span> </a></p>
+                    <li><a href="#" style="color: #4a5c68;">Upload</a>
+                    <li><a href="#" style="color: #4a5c68;">Sign Out</a></li>
+                    <li><a href="#" style="color: #4a5c68;">Edit Profile</a></li>
+
+
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="categories">
+        <center>
+            <i><h1>Categories</h1></i>
+            <p> <a href="Home.php">PHOENIX, AZ <span class="glyphicon glyphicon-map-marker"> </span> </a></p>
+        </center>
+        <a href="categories/auto.php">
+            <div class="categoriefill">
+                <input type="button" class="button" value="Auto" />
+            </div>
+        </a>
+
+        <a href="categories/clothing.php">
+            <div class="categoriefill">
+                <input type="button" class="button" value="Clothing" />
+            </div>
+        </a>
+
+        <a href="categories/technology.php">
+            <div class="categoriefill">
+                <input type="button" class="button" value="Technology" />
+            </div>
+        </a>
+
+        <a href="categories/household.php">
+            <div class="categoriefill">
+                <input type="button" class="button" value="Household" />
+            </div>
+        </a>
+
+        <a href="categories/games.php">
+            <div class="categoriefill">
+                <input type="button" class="button" value="Games" />
+            </div>
+        </a>
+
+        <a href="categories/tools.php">
+            <div class="categoriefill">
+                <input type="button" class="button" value="Tools" />
+            </div>
+        </a>
+
+        <a href="categories/sport.php">
+            <div class="categoriefill">
+                <input type="button" class="button" value="Sport" />
+            </div>
+        </a>
+    </div>
+
+    <div class="profile">
+        <?php
+        if (@$_SESSION['signIn'] == true) {
+            $profileImage = $_SESSION['profileImage'];
+            echo "<center>";
+            echo '<img class="profileimg" src="images/' . $profileImage . '" >';
+            echo "</center>";
+            echo '<a href="profile.php">Profile</a>';
+            echo '<a href="editProfile.php">Edit Profile</a>';
+            echo '<a href="signOut.php">Sign Out</a>';
+            echo '<a href="signUp.php">Sign Up</a>';
+            echo '<a href="upload.php">Upload</a>';
+        }
+        else {
+            echo "<center>";
+            echo '<img class="profileimg" src="images/default-avatar.png" >';
+            echo "</center>";
+            echo '<a href="signIn.php">Sign In</a>';
+            echo '<a href="signUp.php">Sign Up</a>';
+        }
+        ?>
+    </div>
 
     <?php
         // Establish Connections and Sessions
