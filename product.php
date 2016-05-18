d8a3d9e578493c15dd013136289379cfdc1ec0e1
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-05-18 13:50:40 -0700

    product page css

--- product.php
+++ product.php
@@ -71,49 +71,133 @@
     <link rel="stylesheet" href="stylesheet.css">
     <link async href="http://fonts.googleapis.com/css?family=Advent%20Pro" data-generated="http://enjoycss.com" rel="stylesheet" type="text/css"/>
 </head>
-    <body>
-        <?php
-        $imagePath = "images/" . $productImage;
-        echo '<div class="productholder">';
-        echo '<div class="imgholder">';
-        echo '<img src="' . $imagePath . '" class="productimage">';
-        echo "</div>";
-        echo '<div class="nameholder">';
-        echo '<p><b>Name: </b>' . $productName . '</p>';
-        echo '</div>';
-        echo '<div class="priceholder">';
-        echo '<p><b>Price: </b>' . $productPrice . '</p>';
-        echo '</div>';
-        echo '<div class="likeholder">';
-        echo '<p><b>Likes: </b>' . $productLikes . '</p>';
-        echo '</div>';
-        echo '<div class="categoryholder">';
-        echo '<p><b>Category: </b>' . $productCatagory . '</p>';
-        echo '</div>';
-        echo '<div class="descriptionholder">';
-        echo '<p><b>Description: </b>' . $productDescription . '</p>';
-        echo '</div>';
-        echo '<div class="buttonholder">';
-        echo '<form method="post" name="addProduct">';
-        echo '<input type="hidden" name="productidCart" value="' . $productid . '">';
-        echo '<button type="submit" class="button1" name="addProduct" value="1">Add to Cart</button>';
-        echo '</form>';
-        echo '<form method="post" name="likeProduct">';
-        echo '<input type="hidden" name="productidLike" value="' . $productid . '">';
-        echo '<button type="submit" class="button2" name="likeProduct" value="1">Like</button>';
-        echo '</form>';
-        echo '</div>';
-        echo "</div>";
-        ?>
+<body>
+    <div class="casingnav">
+        <div class="topbar">
+            <div class="casingnav">
+                <ul>
+                    <a href="index.php"><img src="images/SwiftSell.png" style="height:58px; float: left"> </a>
+                    <p class="locationlink" style="margin-left: 10px;"><a href="Home.php">Phoenix, Az <span class="glyphicon glyphicon-map-marker"> </span> </a></p>
+                    <li><a href="#" style="color: #4a5c68;">About Us</a>
+                    <li><a href="#" style="color: #4a5c68;">Locations</a></li>
 
-        <form method="post" name="likeProduct">
-            <button type="submit" name="likeProduct" value="1">Like</button>
-        </form>
+                </ul>
+                </div>
+            </div>
+        </div>
 
-        <form method="post" name="addCart">
-            <button type="submit" name="addCart" value="1">Add to Cart</button>
-        </form>
+        <div class="categories">
+            <center>
+                <i><h1>Categories</h1></i>
+                <p> <a href="Home.php">PHOENIX, AZ <span class="glyphicon glyphicon-map-marker"> </span> </a></p>
+            </center>
+            <a href="categories/auto.php">
+                <div class="categoriefill">
+                    <input type="button" class="button" value="Auto" />
+                </div>
+            </a>
 
-        <a href="checkout.php">Checkout</a>
-    </body>
+            <a href="categories/clothing.php">
+                <div class="categoriefill">
+                    <input type="button" class="button" value="Clothing" />
+                </div>
+            </a>
+
+            <a href="categories/technology.php">
+                <div class="categoriefill">
+                    <input type="button" class="button" value="Technology" />
+                </div>
+            </a>
+
+            <a href="categories/household.php">
+                <div class="categoriefill">
+                    <input type="button" class="button" value="Household" />
+                </div>
+            </a>
+
+            <a href="categories/games.php">
+                <div class="categoriefill">
+                    <input type="button" class="button" value="Games" />
+                </div>
+            </a>
+
+            <a href="categories/tools.php">
+                <div class="categoriefill">
+                    <input type="button" class="button" value="Tools" />
+                </div>
+            </a>
+
+            <a href="categories/sport.php">
+                <div class="categoriefill">
+                    <input type="button" class="button" value="Sport" />
+                </div>
+            </a>
+        </div>
+
+        <div class="profile">
+            <?php
+                if (@$_SESSION['signIn'] == true) {
+                    $profileImage = $_SESSION['profileImage'];
+                    echo "<center>";
+                        echo '<img class="profileimg" src="images/' . $profileImage . '" >';
+                    echo "</center>";
+                    echo '<a href="profile.php">Profile</a>';
+                    echo '<a href="editProfile.php">Edit Profile</a>';
+                    echo '<a href="signOut.php">Sign Out</a>';
+                    echo '<a href="signUp.php">Sign Up</a>';
+                    echo '<a href="upload.php">Upload</a>';
+                }
+                else {
+                    echo "<center>";
+                        echo '<img class="profileimg" src="images/default-avatar.png" >';
+                    echo "</center>";
+                    echo '<a href="signIn.php">Sign In</a>';
+                    echo '<a href="signUp.php">Sign Up</a>';
+                }
+            ?>
+        </div>
+
+
+        <div class="categorybody">
+
+
+            <div class="productsContainer">
+                <?php
+                $imagePath = "images/" . $productImage;
+                echo '<div class="productholderBig">';
+                echo '<div class="imgholderBig">';
+                echo '<img src="' . $imagePath . '" class="productimageBig">';
+                echo "</div>";
+                echo '<div class="nameholderBig">';
+                echo '<p><b>Name: </b>' . $productName . '</p>';
+                echo '</div>';
+                echo '<div class="priceholderBig">';
+                echo '<p><b>Price: </b>' . $productPrice . '</p>';
+                echo '</div>';
+                echo '<div class="likeholderBig">';
+                echo '<p><b>Likes: </b>' . $productLikes . '</p>';
+                echo '</div>';
+                echo '<div class="categoryholderBig">';
+                echo '<p><b>Category: </b>' . $productCatagory . '</p>';
+                echo '</div>';
+                echo '<div class="descriptionholderBig">';
+                echo '<p><b>Description: </b>' . $productDescription . '</p>';
+                echo '</div>';
+                echo '<div class="buttonholderBig">';
+                echo '<form method="post" name="addProduct">';
+                echo '<input type="hidden" name="productidCart" value="' . $productid . '">';
+                echo '<button type="submit" class="button1" name="addProduct" value="1">Add to Cart</button>';
+                echo '</form>';
+                echo '<form method="post" name="likeProduct">';
+                echo '<input type="hidden" name="productidLike" value="' . $productid . '">';
+                echo '<button type="submit" class="button2" name="likeProduct" value="1">Like</button>';
+                echo '</form>';
+                echo '</div>';
+                echo "</div>";
+                ?>
+                <a href="checkout.php">Checkout</a>
+
+            </div>
+        </div>
+</body>
 </html>
